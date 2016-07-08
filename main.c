8b4f79cd825df22ed800ad716595401ba4af4e8c
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-07-08 13:49:27 +0600

    synchronous test

--- main.c
+++ main.c
@@ -23,7 +23,9 @@
 
 #define CLOCK CLOCK_MONOTONIC
 
-#define STATS
+//#define ASYNC
+
+//#define STATS
 #ifdef STATS
 #define STATS_SIZE 0x100000 // 2^20
 #endif // STATS
@@ -72,6 +74,12 @@ quit:
 	return ret;
 }
 
+void frame_init(struct can_frame *frame) {
+	frame->can_id = 0xf;
+	frame->can_dlc = 1;
+	frame->data[0] = 0xff;
+}
+
 int done = 0;
 
 void sighandler(int signo) {
@@ -103,10 +111,8 @@ void *send_main(void *data) {
 	param.sched_priority = 80;
 	pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
 #endif
-	
-	frame.can_id = 0xf;
-	frame.can_dlc = 1;
-	frame.data[0] = 0xff;
+
+	frame_init(&frame);
 	
 	ts.tv_sec = 0;
 	ts.tv_nsec = NS_SEC/FREQ;
@@ -187,9 +193,6 @@ close:
 
 int main(int argc, char *argv[]) {
 	int fds, fdr, ret = 0;
-	struct timespec lts;
-	struct cookie cookie;
-	pthread_t send_thread, recv_thread;
 	
 #ifdef STATS
 	FILE *file;
@@ -218,20 +221,114 @@ int main(int argc, char *argv[]) {
 	}
 	
 	printf("min\tmax\tavg\tcurrent\n");
-	
+
+#ifdef ASYNC
+	struct timespec lts;
+	struct cookie cookie;
+	pthread_t send_thread, recv_thread;
+
 	cookie.lts = &lts;
 	cookie.fdr = fdr;
 	cookie.fds = fds;
 	cookie.allow = 1;
 #ifdef STATS
 	cookie.stats = stats;
-#endif // STATS	
-		
+#endif // STATS
+
 	pthread_create(&recv_thread, NULL, recv_main, &cookie);
 	pthread_create(&send_thread, NULL, send_main, &cookie);
 	
 	pthread_join(send_thread, NULL);
 	pthread_join(recv_thread, NULL);
+#else // ASYNC
+
+	int st, allow = 1;
+	struct timespec ts, lts, sleepts;
+	struct can_frame frame;
+
+#ifdef REALTIME
+	struct sched_param param;
+	param.sched_priority = 80;
+	pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+#endif
+
+	sleepts.tv_sec = 0;
+	sleepts.tv_nsec = NS_SEC/FREQ;
+
+	struct timeval tv;
+	fd_set set;
+
+	long ns, cur_ns = 0, avg_ns = 0, max_ns = 0, min_ns = NS_SEC;
+	long counter = 0;
+
+	while(!done) {
+
+		if(allow) {
+			allow = 0;
+
+			nanosleep(&sleepts, NULL);
+
+			frame_init(&frame);
+
+			clock_gettime(CLOCK, &lts);
+			st = send(fds, &frame, sizeof(struct can_frame), 0);
+			if(st < 0) {
+				perror("send(fds)");
+				goto close;
+			} else {
+				// printf("fds send\n");
+			}
+		}
+
+		FD_ZERO(&set);
+		FD_SET(fdr, &set);
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+
+		// printf("fdr select\n");
+		st = select(fdr + 1, &set, NULL, NULL, &tv);
+		if(st < 0) {
+			perror("select(fdr)");
+			goto close;
+		} else if(st == 0) {
+			// printf("fdr timeout\n");
+		} else if(st > 0) {
+			// printf("fdr recv\n");
+
+			st = recv(fdr, &frame, sizeof(struct can_frame), 0);
+			if(st < 0) {
+				perror("recv(fdr)");
+				goto close;
+			}
+
+			clock_gettime(CLOCK, &ts);
+
+			ns = NS_SEC*(ts.tv_sec - lts.tv_sec) + ts.tv_nsec - lts.tv_nsec;
+			avg_ns = (avg_ns*counter + ns)/(counter + 1);
+			cur_ns += ns;
+			if(ns < min_ns)
+				min_ns = ns;
+			if(ns > max_ns)
+				max_ns = ns;
+
+	#ifdef STATS
+			stats[counter] = ns;
+			if(counter + 1 >= STATS_SIZE) {
+				done = 1;
+			}
+	#endif // STATS
+
+			++counter;
+			if(!(counter % FREQ)) {
+				printf("%ld\t%ld\t%ld\t%ld\n", min_ns, max_ns, avg_ns, cur_ns/FREQ);
+				cur_ns = 0;
+			}
+
+			allow = 1;
+		}
+	}
+
+#endif // ASYNC
 	
 close:
 	if(fds >= 0)
